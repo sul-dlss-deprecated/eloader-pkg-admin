935cc87a2d8c8b300549ee633f23896e609fc5b7
Joshua Greben <jgreben@stanford.edu>
2016-08-24 14:37:07 -0700

    Changed seeds file to match AuthorizedUsers model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AuthorizedUser.delete_all
-AuthorizedUser.create!(sunet_id: 'jgreben', name: 'Josh Greben')
-AuthorizedUser.create!(sunet_id: 'sdoljack', name: 'Shelley Doljack')
-AuthorizedUser.create!(sunet_id: 'dlrueda', name: 'Daaaaaarsi!')
+AuthorizedUsers.delete_all
+AuthorizedUsers.create!(sunet_id: 'jgreben', name: 'Josh Greben')
+AuthorizedUsers.create!(sunet_id: 'sdoljack', name: 'Shelley Doljack')
+AuthorizedUsers.create!(sunet_id: 'dlrueda', name: 'Daaaaaarsi!')
