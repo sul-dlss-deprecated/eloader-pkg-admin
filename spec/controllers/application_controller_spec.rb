bceb878cc571aa9d2050bcca13973699e7dcf9f6
Joshua Greben <jgreben@stanford.edu>
2016-08-26 00:07:44 -0700

    Test for current_user being an AuthorizedUser to complete code coverage

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -11,4 +11,18 @@ describe ApplicationController do
       expect(user).to eq('testuser')
     end
   end
+  describe '#sunet_id' do
+    it 'should return a user_id when there is a user in the environment' do
+      user = controller.send(:sunet_id)
+      expect(user).to eq('testuser')
+    end
+  end
+  describe '#current_user' do
+    it 'should set the current_user as a webauth_user' do
+      user = FactoryGirl.create(:authorized_users)
+      expect(user.sunet_id).to eq('testuser')
+      expect(user.name).to eq('Test User')
+      expect(controller.send(:current_user)).to be_a AuthorizedUsers
+    end
+  end
 end
