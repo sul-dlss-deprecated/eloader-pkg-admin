d07db7d8839576e56d63ddf5dc7e786367d1f5fc
Shelley Doljack <sdoljack@stanford.edu>
2016-09-01 16:46:47 -0700

    Permits package attributes on creation.

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -73,6 +73,6 @@ class PackagesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def package_params
-    params.fetch(:package, {})
+    params.require(:package).permit!
   end
 end
