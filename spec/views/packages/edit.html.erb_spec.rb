6d3a2f57b7a2ce3d987464da734483478248edb5
Shelley Doljack <sdoljack@stanford.edu>
2016-09-01 15:34:40 -0700

    Autocorrects rubocop offenses.

--- spec/views/packages/edit.html.erb_spec.rb
+++ spec/views/packages/edit.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "packages/edit", type: :view do
+RSpec.describe 'packages/edit', type: :view do
   before(:each) do
-    @package = assign(:package, Package.create!())
+    @package = assign(:package, Package.create!)
   end
-  it "renders the edit package form" do
+  it 'renders the edit package form' do
     render
 
-    assert_select "form[action=?][method=?]", package_path(@package), "post" do
+    assert_select 'form[action=?][method=?]', package_path(@package), 'post' do
     end
   end
 end
